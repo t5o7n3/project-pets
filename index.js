17e729aad3db19725bdc35316be4251b95bc83e2
t5o7n3 <cotne14@gmail.com>
2021-08-01 23:03:29 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const petList = document.querySelector('.pet-list');
 
 
-const statement1 = new Statement('ჩუქდება თეთრი კნუტი 1 თვის','ლუკა','69999987978456','თბილისი','არის საყვარელი', null, 0, 'https://images.unsplash.com/photo-1561389881-0dc69054475b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80');
+const statement1 = new Statement('ჩუქდება თეთრი კნუტი 1 თვის','ლუკა','69999988978456','თბილისი','არის საყვარელი', null, 0, 'https://images.unsplash.com/photo-1561389881-0dc69054475b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80');
 
 const statement2 = new Statement('ჩუქდება თეთრი კნუტი 5 თვის','ცოტნე','99999999999956','რუსთავი','არის ცელქი და საყვარელი', null, 0, 'https://i.pinimg.com/originals/1d/5c/8e/1d5c8e2a6eb1af906f19ebd68bcecc91.jpg');
 
